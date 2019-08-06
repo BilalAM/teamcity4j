825bca024af83c71cfb7738a3303c4e39449f2d6
BilalAM <bilal.asif.97@gmail.com>
2019-08-06 23:12:35 +0500

    Added start and end date time of build .

--- src/main/java/bilal/teamcity4j/core/TeamCityBuild.java
+++ src/main/java/bilal/teamcity4j/core/TeamCityBuild.java
@@ -1,5 +1,7 @@
 package bilal.teamcity4j.core;
 
+import java.time.LocalDateTime;
+
 public class TeamCityBuild {
 
         private String buildId;
@@ -7,6 +9,9 @@ public class TeamCityBuild {
         private String status;
         private String state;
         private String webUrl;
+        private LocalDateTime startDate;
+
+        private LocalDateTime endDate;
 
         public String getBuildId() {
                 return buildId;
@@ -46,6 +51,22 @@ public class TeamCityBuild {
 
         public void setWebUrl(String webUrl) {
                 this.webUrl = webUrl;
+        }
+
+        public LocalDateTime getStartDate() {
+                return startDate;
+        }
+
+        public void setStartDate(LocalDateTime startDate) {
+                this.startDate = startDate;
+        }
+
+        public LocalDateTime getEndDate() {
+                return endDate;
+        }
+
+        public void setEndDate(LocalDateTime endDate) {
+                this.endDate = endDate;
         }
 
 
