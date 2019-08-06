ca4b2c43ceb3c51ca8adf0f4b9c73251dd30f364
BilalAM <bilal.asif.97@gmail.com>
2019-08-06 23:54:52 +0500

    License Added with name and date .

--- src/main/java/bilal/teamcity4j/core/TeamCityParser.java
+++ src/main/java/bilal/teamcity4j/core/TeamCityParser.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2019 Bilal Asif Mirza
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package bilal.teamcity4j.core;
 
 import org.dom4j.Element;
@@ -11,10 +34,8 @@ import java.time.format.DateTimeFormatter;
  */
 public class TeamCityParser {
 
-        private static final DateTimeFormatter DATE_TIME_FORMATTER_RAW = DateTimeFormatter
-                .ofPattern("yyyyMMdd'T'HHmmssZ");
-        private static final DateTimeFormatter DATE_TIME_FORMATTER_PRETTY = DateTimeFormatter
-                .ofPattern("yyyy-MM-dd HH:mm:ss");
+        private static final DateTimeFormatter DATE_TIME_FORMATTER_RAW = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmssZ");
+        private static final DateTimeFormatter DATE_TIME_FORMATTER_PRETTY = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         public TeamCityProject getSingleProjectFromSingleNode(Node projectNode) {
                 TeamCityProject project = new TeamCityProject();
@@ -54,12 +75,10 @@ public class TeamCityParser {
                 TeamCityBuild teamCityBuild = new TeamCityBuild();
                 try {
                         Element buildHistoryElement = (Element) buildHistoryNode;
-                        LocalDateTime startDateRaw = LocalDateTime
-                                .parse(buildHistoryElement.element("startDate").getStringValue(),
-                                        DATE_TIME_FORMATTER_RAW);
-                        LocalDateTime endDateRaw = LocalDateTime
-                                .parse(buildHistoryElement.element("finishDate").getStringValue(),
-                                        DATE_TIME_FORMATTER_RAW);
+                        LocalDateTime startDateRaw = LocalDateTime.parse(buildHistoryElement.element("startDate").getStringValue(),
+                                DATE_TIME_FORMATTER_RAW);
+                        LocalDateTime endDateRaw = LocalDateTime.parse(buildHistoryElement.element("finishDate").getStringValue(),
+                                DATE_TIME_FORMATTER_RAW);
 
                         teamCityBuild.setStartDate(startDateRaw.format(DATE_TIME_FORMATTER_PRETTY));
                         teamCityBuild.setEndDate(endDateRaw.format(DATE_TIME_FORMATTER_PRETTY));
--- src/main/java/testing/ClientTest.java
+++ src/main/java/testing/ClientTest.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2019 Bilal Asif Mirza
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package testing;
 
 import bilal.teamcity.TeamCityHelper;
@@ -6,9 +29,6 @@ import bilal.teamcity4j.core.TeamCityProject;
 public class ClientTest {
         public static void main(String[] args) {
                 TeamCityProject teamCityProject = TeamCityHelper.getProject("SelfErp");
-                System.out.println("dsdsds");
-                //   DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmssZ");
-                //  LocalDateTime a=  LocalDateTime.parse("20190605T193716+0500",formatter);
-                //   System.out.println(a.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
         }
 }
